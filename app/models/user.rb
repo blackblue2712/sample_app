70046149eb01299a18f8594798b56323858e9a4d
blackblue2712 <lars27399@gmail.com>
2019-10-18 17:58:09 +0700

    Add account activation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,9 @@
 class User < ApplicationRecord
-    attr_accessor :remember_token
+    attr_accessor :remember_token, :activation_token
 
-    # before_save { self.email = email.downcase }
-    before_save { email.downcase! } 
+    before_save :downcase_email
+    before_create :create_activation_digest
+
     validates :name, presence: true, length: { maximum: 50 }
     VALID_EMAIL_REGEX = /\A[\w+\-.]+@([a-zA-Z0-9]+\.){0,}[a-zA-Z0-9]+\.[a-zA-Z0-9]+\z/i
     validates :email, presence: true, length: { maximum: 225 },
@@ -32,14 +33,37 @@ class User < ApplicationRecord
     end
 
     # Returns true if the given token matches the digest.
-    def authenticated?(remember_token)
+    def authenticated?(attribute, token)
+        
         # remember_digest is self.remeber_digest, is created automatically by Active Record based on the name of the corresponding database column
-        return false if self.remember_digest.nil?
-        BCrypt::Password.new(remember_digest).is_password?(remember_token)
+        digest = self.send("#{attribute}_digest")
+        return false if digest.nil?
+        BCrypt::Password.new(digest).is_password?(token)
     end
 
     def forget
         self.update_attribute(:remember_digest, nil)
     end
+
+    # Activates an account.
+    def activate
+        update_columns(activated: true, activated_at: Time.zone.now)
+    end
+
+    # Sends activation email.
+    def send_activation_email
+        UserMailer.account_activation(self).deliver_now
+    end
+
+    private
+        def downcase_email
+            self.email.downcase!
+        end
+
+        # Creates and assigns the activation token and digest
+        def create_activation_digest
+            self.activation_token = User.new_token;
+            self.activation_digest = User.digest(activation_token)
+        end
     
 end
--- config/environments/test.rb
+++ config/environments/test.rb
@@ -39,10 +39,12 @@ Rails.application.configure do
   # The :test delivery method accumulates sent emails in the
   # ActionMailer::Base.deliveries array.
   config.action_mailer.delivery_method = :test
+  config.action_mailer.default_url_options = { host: '127.0.0.1:3000' }
 
   # Print deprecation notices to the stderr.
   config.active_support.deprecation = :stderr
 
   # Raises error for missing translations.
   # config.action_view.raise_on_missing_translations = true
+  
 end
