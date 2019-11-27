655fecd8995d4510584016a92ae91651f7ad1312
blackblue2712 <lars27399@gmail.com>
2019-11-27 17:07:06 +0700

    leave send mail

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -26,10 +26,9 @@ class UsersController < ApplicationController
       # flash[:success] = "Welcome to the Sample App"
       # redirect_to @user
 
-      @user.send_activation_email
+      # @user.send_activation_email
       flash[:info] = "Please check your email to activate your account."
       redirect_to root_url
-
     else
       render 'new'
     end
